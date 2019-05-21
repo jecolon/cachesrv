8749890debfe6cb195fbdc83c993c059ac8bc058
Jose Colon Rodriguez <jec.rod@gmail.com>
2019-05-20 21:10:35 -0400

    Added Get, Put, and Del tests

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"context"
 	"crypto/tls"
+	"flag"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
 	"sync"
 
 	"github.com/jecolon/mcache"
@@ -38,21 +42,63 @@ func (cs *cacheServer) Del(_ context.Context, e *mcache.Entry) (*mcache.Entry, e
 	return e, nil
 }
 
+var puerto = flag.String("p", ":8888", "Dirección IP y puerto")
+var dev = flag.Bool("d", false, "Modo de desarrollo local")
+
 func main() {
-	cer, err := tls.LoadX509KeyPair("tls/dev/cert.pem", "tls/dev/key.pem")
+	flag.Parse()
+
+	// Lanzamos el server gRPC
+	srv := grpc.NewServer()
+	go startServer(srv)
+
+	// Velamos por señal para detener el server
+	conxCerradas := make(chan struct{})
+	go waitForShutdown(conxCerradas, srv)
+
+	// Esperamos a que el shut down termine al cerrar todas las conexiones.
+	<-conxCerradas
+	fmt.Println("Shut down del servidor mcache gRPC completado exitosamente.")
+}
+
+func startServer(srv *grpc.Server) {
+	certFile, keyFile := "tls/prod/cert.pem", "tls/prod/key.pem"
+	if *dev {
+		certFile, keyFile = "tls/dev/cert.pem", "tls/dev/key.pem"
+	}
+	cer, err := tls.LoadX509KeyPair(certFile, keyFile)
 	if err != nil {
 		log.Fatalf("tls.LoadX509KeyPair: %v", err)
 	}
 
 	config := &tls.Config{Certificates: []tls.Certificate{cer}}
-	ln, err := tls.Listen("tcp", ":8888", config)
+	ln, err := tls.Listen("tcp", *puerto, config)
 	if err != nil {
 		log.Fatalf("tls.Listen: %v ", err)
 	}
 	defer ln.Close()
 
-	srv := grpc.NewServer()
+	// Atamos nuestro server a la interface del paquete mcache generada por protoc.
 	mcache.RegisterCacheServer(srv, &cacheServer{store: make(map[string]*mcache.Entry)})
 
-	log.Fatal(srv.Serve(ln))
+	// Iniciamos el server.
+	fmt.Printf("Servidor mcache gRPC en puerto %s listo. CTRL+C para detener.\n", *puerto)
+	if err := srv.Serve(ln); err != nil && err != grpc.ErrServerStopped {
+		// Error iniciando el Server. Posible conflicto de puerto, permisos, etc.
+		log.Printf("Error durante Serve: %v", err)
+	}
+}
+
+func waitForShutdown(conxCerradas chan struct{}, srv *grpc.Server) {
+	// Canal para recibir señal de interrupción.
+	sigint := make(chan os.Signal, 1)
+	// Escuchamos por una señal de interrupción del OS (SIGINT).
+	signal.Notify(sigint, os.Interrupt)
+	<-sigint
+	// Si llegamos aquí, recibimos la señal, iniciamos shut down.
+	// Noten se puede usar un Context para posible límite de tiempo.
+	fmt.Println("\nShut down del servidor mcache gRPC iniciado...")
+	srv.GracefulStop()
+	// Cerramos el canal, señalando conexiones ya cerradas.
+	close(conxCerradas)
 }
--- srv_test.go
+++ srv_test.go
@@ -21,8 +21,17 @@ var (
 )
 
 func setup() {
+	// Asignamos flags manualmente
+	*puerto = "127.0.0.1:8888"
+	*dev = true
+
+	// Lanzamos el server
+	srv := grpc.NewServer()
+	go startServer(srv)
+
+	// Si usamos certificado "self-signed" tenemos que desactivar verificación
 	creds := credentials.NewTLS(&tls.Config{InsecureSkipVerify: true})
-	c, err := grpc.Dial(":8888", grpc.WithTransportCredentials(creds))
+	c, err := grpc.Dial(*puerto, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatalf("grpc.Dial: %v", err)
 	}
@@ -35,6 +44,60 @@ func setup() {
 		Mtime:   time.Now().UTC().UnixNano(),
 		Content: body,
 		Size:    int64(len(body)),
+	}
+
+	// Give the server a chance to start
+	time.Sleep(3 * time.Second)
+}
+
+func equal(ne, e mcache.Entry) bool {
+	if ne.Key != e.Key {
+		return false
+	}
+	if ne.Ctype != e.Ctype {
+		return false
+	}
+	if ne.Mtime != e.Mtime {
+		return false
+	}
+	if ne.Size != e.Size {
+		return false
+	}
+	for i := range ne.Content {
+		if ne.Content[i] != e.Content[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestPutGet(t *testing.T) {
+	setupOnce.Do(setup)
+	
+	if _, err := client.Put(context.Background(), &e); err != nil {
+		t.Fatalf("Error in Put: %v", err)
+	}
+	ne, err := client.Get(context.Background(), &mcache.Entry{Key: "foo.txt"})
+	if err != nil {
+		t.Fatalf("Error in Get: %v", err)
+	}
+	if !equal(*ne, e) {
+		t.Fatalf("ne: %v != e: %v", ne, e)
+	}
+}
+
+func TestDel(t *testing.T) {
+	setupOnce.Do(setup)
+	
+	if _, err := client.Put(context.Background(), &e); err != nil {
+		t.Fatalf("Error in Put: %v", err)
+	}
+	if _, err := client.Del(context.Background(), &mcache.Entry{Key: "foo.txt"}); err != nil {
+		t.Fatalf("Error in Del: %v", err)
+	}
+	ne, err := client.Get(context.Background(), &mcache.Entry{Key: "foo.txt"})
+	if err == nil {
+		t.Fatalf("No error in Get after Del, got: %v", ne)
 	}
 }
 
